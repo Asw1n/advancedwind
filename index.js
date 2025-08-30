3df0b9166377f4d30c8169ffb8361ab3768315b0
Aswin Bouwmeester <aswin.bouwmeester@gmail.com>
2025-08-30 23:05:47 +0200

    version 2.2.0

--- index.js
+++ index.js
@@ -254,7 +254,8 @@ module.exports = function (app) {
 
     //apparent wind
     apparentWind = new Polar("apparentWind", "environment.wind.speedApparent", "environment.wind.angleApparent", options.apparentWindSource, options.apparentWindSource);
-    apparentWind.subscribe(app, plugin.id, true, true, !options.preventDuplication, missingData);
+    // bug in logic to prevent duplication, temporary disabled
+    apparentWind.subscribe(app, plugin.id, true, true, false &&!options.preventDuplication, missingData);
     apparentWindStat = new PolarDamped("apparentWind", apparentWind, options.timeConstant, options.timeConstant);
     apparentWind.onChange = () => { apparentWindStat.sample(); }
     apparentWindStat.setDisplayAttributes({ label: "Apparent Wind", plane:"Boat" });
